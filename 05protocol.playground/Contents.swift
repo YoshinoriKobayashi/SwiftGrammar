046ce8a60058fe83f3cdef0bd71d8c187a4e286a
Yoshinori Kobayashi <yoshinori.kobayashi@music.yamaha.com>
2020-09-15 09:34:44 +0900

    プロトコルの追加と修正

--- 05protocol.playground/Contents.swift
+++ 05protocol.playground/Contents.swift
@@ -59,9 +59,98 @@ struct Mob : Human {
 }
 
 // プロトコルは型として使える
+// プロトコルHumanを配列として引数に使う。
+// 名前の一覧を表示する
 func printNames(list:[Human]) {
     for p in list {
         print((p.name ?? "名無し") + "さん")
-        p.cheer()
+//        p.cheer()               // 呼び出しはできない
     }
 }
+
+// プロトコルの継承
+// プロトコルは別のプロトコルを継承して宣言することができる
+// プロトコルHumanを継承して、役職などの肩書きプロパティに持つプロトコルStaffを定義する
+protocol Staff : Human {
+    // 読み書き可能なプロトコル
+    var title: String{ get set }
+}
+
+// プロトコルの合成
+// 複数のプロトコルを指定すると和集合のようになる
+
+// protocol NewProtocol:プロトコル1,プロトコル2 {} 
+// var teller: NewProtocol
+
+// 以下も同じ意味
+// var teller: プロトコル1 & プロトコル2
+
+
+// プロトコルの使用例
+
+// プロトコルHumanとStaffの両方で採用されたプロパティとメソッドが利用できる。
+// プロトコルStaffを採用
+struct Employee: Staff {
+    let name: String?            // プロトコルHumanのプロパティ 
+    var title:String = ""        // プロトコルStaffのプロパティ
+    func sayHello(to p:Human) {  // プロトコルHumanのメソッド
+        var s = "ご苦労さまです"
+        if let who = p.name { s += "、" + who + "さん"}
+        print(s)
+    }
+}
+
+
+protocol FullName {
+    var lastName: String { get }    // 苗字
+    var firstName: String { get }   // 名前 
+}
+
+struct Student: Human, FullName {
+    let lastName, firstName: String
+    var name: String? {                 // 計算型プロパティで実装
+        return lastName + firstName     // FullNameのプロパティ
+    }
+    func sayHello(to p: Human) {          // プロトコルHumanのメソッド
+        var s = "こんにちは"
+        if let who = p.name { s += "、" + who + "さん"}
+        print(s)
+    }
+}
+
+var a = Mob()
+var b = Employee(name:"安岡", title: "ボディガード")
+var c = Student(lastName: "夏川", firstName: "真那")
+a.sayHello(to: b)   // どうも
+b.sayHello(to: c)   // ご苦労様です、夏川真那さん
+c.sayHello(to: a)   // こんにちは
+
+
+// プロトコルと付属型
+
+// プロトコルでは、そのプロトコルを採用する型定義の内部で使われる付属型（ネスト型）に関して宣言することができる。
+// associatedtypeというキーワードを使う
+
+protocol SimpleVector {
+    associatedtype Element          //　付属型の宣言、ジェネリクス
+    var x : Element { get set }
+    var y : Element { get set }
+}
+// 特定の型が指定されていない付属型の宣言は型パラメータとして機能する
+
+struct VectorFloat : SimpleVector {
+    typealias Element = Float       // 具体的な型を別名で指定
+    var x,y : Float
+}
+struct VectorDouble : SimpleVector, CustomStringConvertible {
+    var x,y: Double         //　具体的な型は推論される
+    var description: String { return "[\(x),\(y)]"}
+    
+    init(x: VectorDouble.Element, y:VectorDouble.Element) {
+        self.x = x; self.y = y          // 付属型を型として使うことも可能
+    }
+    init(vectorFloat d: VectorFloat) {
+        self.init(x: Double(d.x), y: Double(d.y))
+    }
+}
+
